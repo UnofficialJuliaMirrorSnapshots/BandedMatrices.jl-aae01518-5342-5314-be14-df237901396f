938115567bcdb22c2d027aa3f2b0ec363df39474
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-25 09:57:46 +0000

    Snapshot of branch master taken on 2019-09-25T05:57:45.825-04:00 from "https://github.com/UnofficialJuliaMirror/BandedMatrices.jl-aae01518-5342-5314-be14-df237901396f"

--- src/interfaceimpl.jl
+++ src/interfaceimpl.jl
@@ -107,6 +107,8 @@ isbanded(M::MulMatrix) = isbanded(Applied(M))
 const MulBandedLayout = MulLayout{<:Tuple{Vararg{<:AbstractBandedLayout}}}
 
 applybroadcaststyle(::Type{<:AbstractMatrix}, ::MulBandedLayout) = BandedStyle()
+applybroadcaststyle(::Type{<:AbstractMatrix}, ::MulLayout{<:Tuple{BandedColumns{LazyLayout},Vararg{<:AbstractBandedLayout}}}) = LazyArrayStyle{2}()
+BroadcastStyle(::BandedStyle, ::LazyArrayStyle{2}) = LazyArrayStyle{2}()
 
 @inline colsupport(::MulBandedLayout, A, j) = banded_colsupport(A, j)
 @inline rowsupport(::MulBandedLayout, A, j) = banded_rowsupport(A, j)
@@ -164,11 +166,16 @@ function resizedata!(B::CachedMatrix{T,BandedMatrix{T,Matrix{T},OneTo{Int}}}, n:
     olddata = B.data
     ν,μ = size(olddata)
     n,m = max(ν,n), max(μ,m)
+    l,u = bandwidths(B.array)
+    λ,ω = bandwidths(B.data)
+
     if (ν,μ) ≠ (n,m)
         B.data = BandedMatrix{T}(undef, (n,m), bandwidths(olddata))
         B.data.data[:,1:μ] .= olddata.data
         bd = bandeddata(B.array)
-        B.data.data[:,μ+1:end] .= view(bd,:,μ+1:m)
+        B.data.data[1:(ω-u),μ+1:end] .= zero(T)
+        B.data.data[ω-u+1:ω+l+1,μ+1:end] .= view(bd,:,μ+1:m)
+        B.data.data[ω+l+2:end,μ+1:end] .= zero(T)
     end
 
     B.datasize = (n,m)
